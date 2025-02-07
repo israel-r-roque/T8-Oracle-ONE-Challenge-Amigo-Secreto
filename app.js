acd2a82c97fc050364a36bcc9b0ec07e743ee791
Israel Roque <ribeiroroqueisrael@gmail.com>
2025-02-07 18:51:53 -0300

    feat(amigo-secreto): Finaliza implementação da aplicação "Amigo Secreto"
    Este commit marca a finalização da implementação da aplicação "Amigo Secreto", incluindo todas as funcionalidades:
    
    - Adicionar nomes à lista.
    - Validar entrada de nomes.
    - Exibir lista de nomes.
    - Sortear amigo secreto aleatoriamente.
    
    A aplicação foi desenvolvida utilizando HTML, CSS e JavaScript, com manipulação do DOM para interação com o usuário. O sorteio aleatório é implementado com a função `Math.random()`.
    
    Este commit inclui os arquivos finais:
    
    - index.html
    - style.css
    - script.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+const listaAmigos = document.getElementById("resultado");
+
+const adicionarAmigo = () => {
+  const amigo = document.getElementById("amigo");
+
+  if (amigo.value != "") {
+    amigos.push(amigo.value);
+    amigo.value = "";
+  } else {
+    alert("Por favor, insira um nome.");
+  }
+
+  atualizarListaAmigos();
+};
+
+const atualizarListaAmigos = () => {
+  listaAmigos.innerHTML = "";
+
+  amigos.forEach((amigo) => {
+    listaAmigos.innerHTML += `<li>${amigo}</li>`;
+  });
+};
+
+const sortearAmigo = () => {
+  if (amigos.length > 0) {
+    const min = 0;
+    const max = amigos.length;
+    const indiceAleatorio = Math.floor(Math.random() * (max - min) + min)
+    listaAmigos.innerHTML = `<li>O amigo sorteado foi: ${amigos[indiceAleatorio]}</li>`
+  }
+}
